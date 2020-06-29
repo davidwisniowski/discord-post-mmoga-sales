77cd5951d20a519a167c8a60ff2c6b4588285e60
David Wisniowski <dwi@comspace.de>
2020-06-29 10:42:46 +0200

    initial commit

---
+++ app.py
@@ -0,0 +1,71 @@
+import discord
+import requests
+import os
+from bs4 import BeautifulSoup
+from dotenv import load_dotenv
+
+# .env file, local environment vars
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN') # bot client token
+GUILD = os.getenv('DISCORD_GUILD') # discord server name
+CHANNEL = os.getenv('DISCORD_CHANNEL') # target channel id
+URL = os.getenv('MMOGA_LINK') # mmoga as sales website
+
+client = discord.Client()
+
+def get_offers():
+    page = requests.get(URL)
+    soup = BeautifulSoup(page.content, 'html.parser')
+    
+    weekend_offers = soup.findAll(True, { 'class':'container', 'class' : 'preorderCont' })
+    #print(weekend_offers, end='\n'*2)
+
+    message = "**MMOGA: Aktuelle Weekend Deals**\n\n"
+
+    for offer in weekend_offers:
+        
+        ## left offer side ##
+        # here we have just one offer
+        link = URL + offer.find('a')['href']
+        title = offer.find('p', class_='title').text
+        oldPrice = offer.find('del', class_='oldPreis').text
+        newPrice = offer.find('span', class_='newPreis').text
+
+        # build message
+        message = message + build_message(title, newPrice, oldPrice, link)
+
+        ## right offer side ##
+        rightOffers = offer.select('.preR li')
+        
+        # here we have multiple offers, so loop
+        for rightOffer in rightOffers:
+            link = URL + rightOffer.find('a')['href']
+            title = rightOffer.find('h4').text
+            oldPrice = rightOffer.find('del').text
+            newPrice = rightOffer.find('p').text
+
+            # build message
+            message = message + build_message(title, newPrice, oldPrice, link)
+
+    return message
+
+def build_message(title, newPrice, oldPrice, link):
+    message = ''
+    message = message + '**' + title + '**' + ' (' + newPrice + ' ~~' + oldPrice.replace("UVP ", "") + '~~)' + '\n'            
+    message = message + 'Link: ' + link
+    message = message + '\n\n'
+    return message
+
+
+@client.event
+async def on_ready():
+    print(f'{client.user} has connected to Discord!')
+
+    guild = discord.utils.get(client.guilds, name=GUILD)
+    channel = guild.get_channel(CHANNEL)
+    message = get_offers()
+    print(message)
+    ##await channel.send(message)
+    os._exit(os.EX_OK)
+
+client.run(TOKEN)
